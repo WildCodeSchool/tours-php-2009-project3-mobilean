66dbb57939da20321850341fc0fb936b132cdc2b
Yann Raulet <yann.raulet@hotmail.fr>
2021-01-26 16:51:35 +0100

    forms switch with javascript

--- assets/js/switchForm.js
+++ assets/js/switchForm.js
@@ -1,23 +1,23 @@
 function myFunction() {
-    let form1 = document.getElementById("formCompany");
-    let form2 = document.getElementById("formParticular");
-    let switchButton = document.getElementById("switch");
+    const form1 = document.getElementById('formCompany');
+    const form2 = document.getElementById('formParticular');
+    const switchButton = document.getElementById('switch');
 
-    if (switchButton.value === "Formulaire - Je suis un particulier") {
-            switchButton.value = " Formulaire - Je suis une entreprise";
-        } else {
-            switchButton.value = "Formulaire - Je suis un particulier";
-        }
-
-    if (form1.style.display === "none") {
-        form1.style.display = "block";
+    if (switchButton.value === 'Formulaire - Je suis un particulier') {
+        switchButton.value = 'Formulaire - Je suis une entreprise';
     } else {
-        form1.style.display = "none";
+        switchButton.value = 'Formulaire - Je suis un particulier';
     }
-    if (form2.style.display === "none") {
-        form2.style.display = "block";
+
+    if (form1.style.display === 'none') {
+        form1.style.display = 'block';
     } else {
-        form2.style.display = "none";
+        form1.style.display = 'none';
+    }
+    if (form2.style.display === 'none') {
+        form2.style.display = 'block';
+    } else {
+        form2.style.display = 'none';
     }
 }
 
