6668f7bf421edab9d042d3b338e18d7434a59080
Yann Raulet <yann.raulet@hotmail.fr>
2021-01-27 12:14:47 +0100

    add css style

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,8 @@ import '../styles/vehicle.scss';
 import '../styles/frontLayout.scss';
 import '../styles/adminCrud.scss';
 import '../styles/estimates.scss';
+import '../styles/partner.scss';
+
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
